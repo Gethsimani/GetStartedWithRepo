5ec6d989369be2936fd5fca14740ea4e84a33ee1
Gesthimani Christidou <126727615+Gethsimani@users.noreply.github.com>
2023-04-18 14:55:56 +0100

    Add files via upload

---
+++ OrStatement/src/OrStatement.java
@@ -0,0 +1,10 @@
+public class OrStatement {
+    public static void main(String[] args) {
+        String gift= "Ring ";
+        if (gift=="Ring" || gift=="Necklace") {
+            System.out.println("I wil marry you!!");
+        } else {
+            System.out.println("You have no chance man");
+        }
+    }
+}
