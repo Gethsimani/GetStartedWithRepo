2708891ed4897670b58d1b2daa28fc7cc115ddb4
Gesthimani Christidou <126727615+Gethsimani@users.noreply.github.com>
2023-04-18 14:26:16 +0100

    Add files via upload

---
+++ Exercise1.java
@@ -0,0 +1,14 @@
+public class Exercise1 {
+    public static void main(String []args){
+        for (int i=1; i<=5; i++) {
+            System.out.println(i+":Never gonna get me, he he!");
+//While Loop
+            int j=1;
+            while (j<=5){
+                System.out.println("Runt to save your Life!!");
+                j++;
+
+            }
+        }
+    }
+}
